56a67684a1bdcd17bf95118d9821bd0feedc8c2c
Luke Sandberg <lukeisandberg@gmail.com>
2025-09-04 23:46:34 -0700

    clippy

--- turbopack/crates/turbo-tasks-backend/src/backend/storage.rs
+++ turbopack/crates/turbo-tasks-backend/src/backend/storage.rs
@@ -830,7 +830,7 @@ impl Storage {
         key1: TaskId,
         key2: TaskId,
     ) -> (StorageWriteGuard<'_>, StorageWriteGuard<'_>) {
-        let (a, b) = get_multiple_mut(&self.map, key1, key2, || InnerStorage::new());
+        let (a, b) = get_multiple_mut(&self.map, key1, key2, InnerStorage::new);
         (
             StorageWriteGuard {
                 storage: self,
